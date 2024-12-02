d6dc291dcd03b56baed706e45b3d27229a733db4
JuanSZ117 <jjeguis@unimagdalena.edu.co>
2024-12-01 19:47:02 -0500

    Mi commit

---
+++ main.js
@@ -0,0 +1,15 @@
+ // Funciones declaradas
+
+ function saludar(nombre){
+    console.log("Te saludo" + nombre);
+ }
+
+ saludar("Juan José");
+
+ // Funcion Anonima
+
+ const saludar = function(nombre){
+    console.log("Te saludo" + " " + nombre);
+ }
+
+ saludar("Juan");
